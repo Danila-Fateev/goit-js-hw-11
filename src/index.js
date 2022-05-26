ad6b5ce2cb51855d8bd00987b1ce2ed5170a0eba
Вадим Фатеев <fateev1vv2@gmail.com>
2022-05-26 23:48:42 +0300

    1

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const API_KEY = '27675022-eae91b965f306fbe1611b8e88';
+const refs = {
+    searchBtn: document.querySelector('#search-btn'),
+    loadBtn: document.querySelector('.load-more'),
+    searchForm: document.querySelector('#search-form'),
+    inputForm: document.querySelector('input-form')
+}
