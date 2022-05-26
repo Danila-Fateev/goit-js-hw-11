f416c3c704935b744768fe1962a10f0b903ab316
Вадим Фатеев <fateev1vv2@gmail.com>
2022-05-27 00:07:01 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,3 +5,14 @@ const refs = {
     searchForm: document.querySelector('#search-form'),
     inputForm: document.querySelector('input-form')
 }
+
+let page = 0;
+
+refs.searchForm.addEventListener('submit', (e) => e.preventDefault());
+refs.searchBtn.addEventListener('click', (e) => {
+    page = 1;
+    const inputValue = refs.inputForm.value;
+    fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${inputValue}&image_type=photo`)
+})
+
+// refs.loadBtn.style.display = 'block';
