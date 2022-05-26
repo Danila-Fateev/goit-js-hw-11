7de6b243e647fee598b1f284733c065a2155f061
Вадим Фатеев <fateev1vv2@gmail.com>
2022-05-27 00:28:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const refs = {
     searchBtn: document.querySelector('#search-btn'),
     loadBtn: document.querySelector('.load-more'),
     searchForm: document.querySelector('#search-form'),
-    inputForm: document.querySelector('input-form')
+    inputForm: document.querySelector('.input-form')
 }
 
 let page = 0;
@@ -12,7 +12,29 @@ refs.searchForm.addEventListener('submit', (e) => e.preventDefault());
 refs.searchBtn.addEventListener('click', (e) => {
     page = 1;
     const inputValue = refs.inputForm.value;
-    fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${inputValue}&image_type=photo`)
+    fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`)
+        .then((r) => {return r.json()})
+        .then((r) => {
+            r.map((el) => {
+                return `<div class="photo-card">
+  <img src="${el.webformatURL}" alt="" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+    </p>
+  </div>
+</div>`
+            })
+    })
 })
 
 // refs.loadBtn.style.display = 'block';
