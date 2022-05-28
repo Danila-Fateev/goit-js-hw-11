2d6b31fcf1305f064b224a5bd2cf779c7ded60cb
Вадим Фатеев <fateev1vv2@gmail.com>
2022-05-28 11:54:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const refs = {
     searchBtn: document.querySelector('#search-btn'),
     loadBtn: document.querySelector('.load-more'),
     searchForm: document.querySelector('#search-form'),
-    inputForm: document.querySelector('.input-form')
+  inputForm: document.querySelector('.input-form'),
+  galleryEl: document.querySelector('.gallery')
 }
 
 let page = 0;
@@ -13,28 +14,33 @@ refs.searchBtn.addEventListener('click', (e) => {
     page = 1;
     const inputValue = refs.inputForm.value;
     fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`)
-        .then((r) => {return r.json()})
+      .then((r) => {
+        if (r.status === 200) {
+          refs.loadBtn.style.display = 'block';
+        }
+        return r.json()
+      })
         .then((r) => {
-            r.hits.map((el) => {
-                return `<div class="photo-card">
-  <img src="${el.webformatURL}" alt="" loading="lazy" />
+           return r.hits.map((el) => {
+              return `<div class="photo-card">
+  <img src="${el.webformatURL}" alt="${el.tags}" loading="lazy" />
   <div class="info">
     <p class="info-item">
-      <b>Likes</b>
+      <b>Likes</b><br>${el.likes}
     </p>
     <p class="info-item">
-      <b>Views</b>
+      <b>Views</b><br>${el.views}
     </p>
     <p class="info-item">
-      <b>Comments</b>
+      <b>Comments</b><br>${el.comments}
     </p>
     <p class="info-item">
-      <b>Downloads</b>
+      <b>Downloads</b><br>${el.downloads}
     </p>
   </div>
 </div>`
-            })
-    })
+            }).join("")
+    }).then((r) => refs.galleryEl.insertAdjacentHTML('beforeend', r))
 })
 
 // refs.loadBtn.style.display = 'block';
